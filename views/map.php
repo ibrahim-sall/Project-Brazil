69922aa4e82c525c068888509bb828d3bd072d6a
thomaswittmer <120556984+thomaswittmer@users.noreply.github.com>
2023-11-17 21:24:02 +0100

    ajout des vues

--- views/map.php
+++ views/map.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <head>
-    <title>Leaflet</title>
+    <title>Escape Game</title>
+    <link rel="icon" type="image/png" href="images/logoF1.png">
     <script src="https://cdn.jsdelivr.net/npm/vue"></script>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="crossorigin=""></script>
+    <script src="https://cdn.jsdelivr.net/npm/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.css" />
+
     <link rel="stylesheet" href="assets/style.css">
     <style>
         body {
@@ -29,7 +33,8 @@
         }
 
         #inventory {
-            background-color: #f0f0f0;
+            background-color: darkgreen;
+            color : white;
             margin : 5px;  
             padding: 40px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
@@ -41,7 +46,7 @@
 
         h2 {
             text-align: center;
-            color: #333; 
+            color: white; 
         }
 
         #inventory-list {
@@ -50,8 +55,8 @@
         }
 
         .selected {
-            border: 2px solid #ffcc00; /* Bordure jaune pour indiquer la sélection */
-            box-shadow: 0 0 5px rgba(255, 204, 0, 0.7); /* Ombre jaune */
+            border: 2px solid yellow; /* Bordure jaune pour indiquer la sélection */
+            background-color: yellow;
         }
 
     </style>
@@ -60,11 +65,15 @@
         <div id="map"></div>
         <div id="inventory">
             <h2>Inventaire</h2>
-            <ul id="inventory-list"></ul>
+            <ul id="inventory-list" class="draggable"></ul>
             <div id="inventory-image"></div>
         </div>
+
+
+        
         
 
         <script src="assets/map.js"></script>
+        <script src="assets/animations.js"></script>
     </body>
 </html>
